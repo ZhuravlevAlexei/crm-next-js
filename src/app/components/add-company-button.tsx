e9760759d1c4d24991214a9de1eb8652b9b90baa
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-10-11 19:27:46 +0300

    navigation

--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 import Button from '@/app/components/button';
-// import CompanyFormModal from './company-form-modal';
 
 const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
   ssr: false,
