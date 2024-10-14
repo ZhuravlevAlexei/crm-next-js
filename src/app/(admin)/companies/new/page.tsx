11c472e335432d2a1d699c69172172fc2603a38e
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-10-14 19:43:40 +0300

    experiment

--- src/app/(admin)/companies/new/page.tsx
+++ src/app/(admin)/companies/new/page.tsx
@@ -6,9 +6,5 @@ import CompanyForm from '@/app/components/company-form';
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
-  return (
-    <div className="py-6 px-10">
-      <CompanyForm />
-    </div>
-  );
+  return <div className="py-6 px-10">{/* <CompanyForm /> */}</div>;
 }
